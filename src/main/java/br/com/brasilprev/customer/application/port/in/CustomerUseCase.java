c8aa62fcdf366df6051ceff7c969291affe4cccb
paiva <paiva@localhost.localdomain>
2021-12-05 23:13:12 -0300

    Porta de entrada para o dominio de negocio da aplicação

--- src/main/java/br/com/brasilprev/customer/application/port/in/CustomerUseCase.java
+++ src/main/java/br/com/brasilprev/customer/application/port/in/CustomerUseCase.java
@@ -2,9 +2,14 @@ package br.com.brasilprev.customer.application.port.in;
 
 import br.com.brasilprev.customer.application.domain.Customer;
 
+import java.util.Optional;
+
 public interface CustomerUseCase {
 	
-	public Customer create(Customer customer);
-	public Customer loadCustomerById(Long id);
+	Optional<Customer> create(Customer customer);
+	Optional<Customer> read(Customer customer);
+	Optional<Customer> update(Customer customer);
+	void delete(Customer customer);
+
 
 }
